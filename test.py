159806ebd0a2ebe740b4846ebb769349f48be044
m3hrab <mehrab.cse16@gmail.com>
2023-11-16 22:31:34 +0600

    test

---
+++ test.py
@@ -0,0 +1,37 @@
+# CP Template 0.2
+# Author: Mehrab (@to0th_less)
+
+import sys
+from math import gcd, sqrt
+from itertools import combinations, permutations
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input Shortcut
+def read_int():
+    return int(sys.stdin.readline().strip())
+
+def read_ints():
+    return map(int, sys.stdin.readline().split())
+
+def read_int_list():
+    return list(map(int, sys.stdin.readline().split()))
+
+# Faster Output Shortcut
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+# Main Function
+def solve():
+    t = read_int()
+    for _ in range(t):
+        pass
+
+
+if __name__ == '__main__':
+    solve()
